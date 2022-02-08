ff3582062403b629dbdd20845a873609b27278b6
bakhromjon <khasanboyevbakhromjon8@gmail.com>
2022-02-08 21:50:38 +0500

    success commit

---
+++ src/main/java/uz/bakhromjon/App.java
@@ -0,0 +1,7 @@
+package uz.bakhromjon;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/uz/bakhromjon/User.java
@@ -0,0 +1,5 @@
+package uz.bakhromjon;
+
+public class User {
+    private  String name;
+}
